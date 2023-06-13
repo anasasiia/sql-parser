43faa647af670192ed4e0fdde89a4ca300b42bf3
anasasiia <sheldoncooperbuga@gmail.com>
2023-06-13 12:17:54 +0400

    updated to Select

--- src/main/java/org/example/ToSelect.java
+++ src/main/java/org/example/ToSelect.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.elements.Column;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,60 +13,54 @@ public class ToSelect {
     public static List<Column> run(String[] arg) {
         List<Column> columns = new ArrayList<>();
 
-        int[] indexOfComma = new int[arg.length / 2 + 1];
         int index = 0;
+        int[] indexOfComma = new int[arg.length / 2 + 1];
         for (int i = 0; i < arg.length; i++) {
             if (arg[i].equals(",")) {
                 indexOfComma[index] = i;
                 index++;
             }
         }
-
-        if (arg.length == 2) {
-            indexOfComma[index] = arg.length - 1;
-        } else {
-            indexOfComma[index] = arg.length;
-        }
+        indexOfComma[index] = arg.length - 1;
+        indexOfComma = Arrays.stream(indexOfComma)
+                .filter(i -> i != 0)
+                .toArray();
 
         int start = 0;
-        if (arg[1].equals("*")) {
-            Column column = new Column();
-            column.setName("all columns");
-            column.setTableAndColumnName(Map.of(arg[1], "empty"));
-            columns.add(column);
-        } else {
-            for (int stop : indexOfComma) {
-                if (stop != 0) {
-                    Column column = new Column();
-                    String name;
-                    Map<String, String> tableAndColumnName = new HashMap<>();
+        int stop;
 
-                    if (stop - (start + 1) > 1) {
-                        name = arg[start + 3];
-                        tableAndColumnName.put(arg[start + 1], "empty");
-                    } else {
-                        name = "one column";
-                        String str = arg[start + 1];
-                        if (arg[start + 1].contains(".") || arg[start + 1].contains("_")) {
-                            int commaIndex = 0;
-                            for (int i = 0; i < str.length(); i++) {
-                                if (str.charAt(i) == '.' || str.charAt(i) == '_') {
-                                    commaIndex = i;
-                                    break;
-                                }
-                            }
-                            tableAndColumnName.put(str.substring(commaIndex + 1),
-                                    str.substring(0, commaIndex));
-                        } else {
-                            tableAndColumnName.put(arg[start + 1], "empty");
-                        }
-                    }
-                    column.setName(name);
-                    column.setTableAndColumnName(tableAndColumnName);
-                    columns.add(column);
-                    start = stop;
+        for (int j : indexOfComma) {
+            stop = j;
+            Column column = new Column();
+            String name;
+            Map<String, String> tableAndColumnName = new HashMap<>();
+
+            if (arg[start + 1].equals("*")) {
+                name = "all columns";
+                tableAndColumnName.put(arg[start + 1], "empty");
+            } else if (stop - (start + 1) > 1) {
+                name = arg[start + 1];
+                tableAndColumnName.put(arg[start + 1], "empty");
+            } else {
+                name = "one column";
+                String str = arg[start + 1];
+                int commaIndex = IntStream.range(0, str.length())
+                        .filter(i -> str.charAt(i) == '.' || str.charAt(i) == '_')
+                        .findFirst()
+                        .orElse(-1);
+
+                if (commaIndex == -1) {
+                    tableAndColumnName.put(arg[start + 1], "empty");
+                } else {
+                    tableAndColumnName.put(str.substring(commaIndex + 1),
+                            str.substring(0, commaIndex));
                 }
             }
+            column.setName(name);
+            column.setTableAndColumnName(tableAndColumnName);
+            columns.add(column);
+
+            start = stop;
         }
         return columns;
     }
