143184cbfc2750bc57a11b2b468f7baefe8dc12b
anasasiia <sheldoncooperbuga@gmail.com>
2023-06-17 13:08:50 +0400

    updated join section

--- src/main/java/org/example/ToJoin.java
+++ src/main/java/org/example/ToJoin.java
@@ -13,13 +13,13 @@ public class ToJoin {
         join.setTable2(arg[2]);
 
         if (!join.getJoinType().equals("CROSS")) {
-            int index1 = arg[4].lastIndexOf('.');
-            String table1 = arg[4].substring(0, index1);
+            int index1 = arg[5].lastIndexOf('.');
+            String table1 = arg[5].substring(0, index1);
             join.setTable1(table1);
-            join.setColumnOfTable1(arg[4].substring(index1 + 1));
+            join.setColumnOfTable1(arg[5].substring(index1 + 1));
 
-            int index2 = arg[6].lastIndexOf('.');
-            join.setColumnOfTable2(arg[6].substring(index2 + 1));
+            int index2 = arg[7].lastIndexOf('.');
+            join.setColumnOfTable2(arg[7].substring(index2 + 1));
         }
         joins.add(join);
     }
