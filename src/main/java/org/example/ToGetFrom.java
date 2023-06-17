14c21d5df4459bec622babe810625578e34a2cc7
anasasiia <sheldoncooperbuga@gmail.com>
2023-06-17 13:07:42 +0400

    updated get from section

--- src/main/java/org/example/ToGetFrom.java
+++ src/main/java/org/example/ToGetFrom.java
@@ -2,51 +2,73 @@ package org.example;
 
 import org.example.elements.Source;
 
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ToGetFrom {
-    public static List<Source> run(String[] arg) throws IOException {
+    public static List<Source> run(String[] arg) throws FileNotFoundException {
         List<Source> fromSources = new ArrayList<>();
-        int[] indexOfComma = new int[arg.length / 2 + 1];
-        int index = 0;
+        List<Integer> indexOfComma = new ArrayList<>();
         for (int i = 0; i < arg.length; i++) {
             if (arg[i].equals(",")) {
-                indexOfComma[index] = i;
-                index++;
+                indexOfComma.add(i);
             }
         }
 
         if (arg.length == 2) {
-            indexOfComma[index] = arg.length - 1;
+            indexOfComma.add(arg.length - 1);
         } else {
-            indexOfComma[index] = arg.length;
+            indexOfComma.add(arg.length);
         }
 
         int start = 0;
-        for (int stop = 0; stop < indexOfComma.length; stop++) {
+        for (Integer stop: indexOfComma) {
             Source source = new Source();
-            if (indexOfComma[stop] != 0) {
-                if (indexOfComma[stop] > 2) {
-                    StringBuilder line = new StringBuilder();
-                    for (int i = 1; i < arg.length - 1; i++) {
-                        arg[i] = arg[i].replace("(", "");
-                        arg[i] = arg[i].replace(")", "");
-                        line.append(arg[i]).append(" ");
-                    }
-                    Parser.parse(line.toString());
-                    Query query = Parser.getQuery();
-                    source.setNestedQuery(query);
-                    source.setAlias(arg[arg.length - 1]);
-                    fromSources.add(source);
-                } else {
-                    source.setTableName(arg[start + 1]);
-                    fromSources.add(source);
+            if (arg[start + 1].equals("(")) {
+                Source source1 = prepareSourceWithNestedQuery(arg, start + 1, arg.length);
+                fromSources.add(source1);
+                break;
+            } else {
+                if (stop - (start + 1) == 2) {
+                    source.setAlias(arg[start + 2]);
                 }
-                start = indexOfComma[stop];
+                source.setTableName(arg[start + 1]);
             }
+            fromSources.add(source);
+
+            if (stop - (start + 1) > 2) {
+                int indexOfOpenBracket = IntStream.range(start, arg.length)
+                        .filter(i -> arg[i].equals("("))
+                        .findFirst()
+                        .orElse(-1);
+                Source source1 = prepareSourceWithNestedQuery(arg, indexOfOpenBracket, arg.length);
+                fromSources.add(source1);
+                break;
+            }
+            start = stop;
         }
         return fromSources;
+    }
+
+    private static Source prepareSourceWithNestedQuery(String[] arg, int indexOfOpenBracket, int lengthOfArg) {
+        Source source = new Source();
+
+        int indexOfCloseBracket = IntStream.range(indexOfOpenBracket, lengthOfArg)
+                .filter(i -> arg[i].equals(")"))
+                .findFirst()
+                .orElse(-1);
+
+        String[] nestedQuery = Arrays.copyOfRange(arg, indexOfOpenBracket + 1, indexOfCloseBracket);
+        int indexOfNestedQuery = Reader.processNestedStatement(nestedQuery);
+
+        source.setIndexOfNestedQuery(indexOfNestedQuery);
+
+        if (lengthOfArg - indexOfCloseBracket > 0) {
+            source.setAlias(arg[indexOfCloseBracket + 1]);
+        }
+        return source;
     }
 }
--- src/main/java/org/example/elements/Source.java
+++ src/main/java/org/example/elements/Source.java
@@ -4,30 +4,26 @@ import org.example.Query;
 
 public class Source {
     private String tableName;
-    private Query nestedQuery;
     private String alias;
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
-    public void setNestedQuery(Query nestedQuery) {
-        this.nestedQuery = nestedQuery;
-    }
-
-    public void setTableName(String tableName) {
-        this.tableName = tableName;
-    }
+    private int indexOfNestedQuery = -1;
 
     public String getTableName() {
         return tableName;
     }
-
-    public Query getNestedQuery() {
-        return nestedQuery;
-    }
-
     public String getAlias() {
         return alias;
     }
+    public int getIndexOfNestedQuery() {
+        return indexOfNestedQuery;
+    }
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+    public void setIndexOfNestedQuery(int indexOfNestedQuery) {
+        this.indexOfNestedQuery = indexOfNestedQuery;
+    }
+
 }
